fc25cdccecbf28041666b9c2e2dd0866240730fd
mkhsojib <mkhsojib@gmail.com>
2017-06-06 12:23:15 +0600

    Relationships and mass assignment

--- app/Staff.php
+++ app/Staff.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
-    //
+
+    protected $fillable = ['name'];
+
+    public function photos()
+    {
+        return $this->morphToMany('App\Photo', 'imageable');
+    }
 }
