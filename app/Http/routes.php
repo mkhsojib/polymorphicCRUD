3e1d4af7f424f779091031ef7d958808e3143c23
mkhsojib <mkhsojib@gmail.com>
2017-06-06 15:05:47 +0600

    data updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,24 +19,39 @@ Route::get('/', function () {
 });
 
 
-Route::get('/create', function (){
+Route::get('/create', function () {
 
-   $staff = Staff::find(1);
+    $staff = Staff::find(1);
 
-   $staff->photos()->create(['path'=>'laravel.jpg']);
+    $staff->photos()->create(['path' => 'laravel.jpg']);
 
 
 });
 
 
-Route::get('/read', function (){
+Route::get('/read', function () {
 
-   $staff = Staff::findOrFail(1);
+    $staff = Staff::findOrFail(1);
 
-   foreach ($staff->photos as $photo){
+    foreach ($staff->photos as $photo) {
 
-       return $photo->path;
-   }
+        return $photo->path;
+    }
+
+});
+
+
+Route::get('/update', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(12)->first();
+
+
+    $photo->path = "update.jpg";
+
+    $photo->save();
+
 
 });
 
